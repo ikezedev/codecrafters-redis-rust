ad0e34404172e666ec391d23017b7d8e33752b36
Ikechukwu Eze <92305839+ikezedev@users.noreply.github.com>
2023-11-21 21:58:49 +0100

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{
+    io::{self, Write},
+    net::{TcpListener, TcpStream},
+};
+
+use thiserror::Error;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,12 +16,22 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_requests(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_requests(mut stream: TcpStream) {
+    let _ = stream.write(b"+PONG\r\n");
+}
+
+#[derive(Error, Debug)]
+enum RedisError {
+    #[error("could not read stream")]
+    ReadStream(#[from] io::Error),
 }
